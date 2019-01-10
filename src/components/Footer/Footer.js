07beb49c9af72b966e17301f28dbe4a5e6a4950a
NBCorrigan <nickb.corrigan@gmail.com>
2019-01-10 01:51:57 -0800

    initial code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Nick Corrigan</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import Farrah from './images/Farrah.png';
+import Rupaul from './images/Rupaul.jpg';
+import Trixie from './images/Trixie.jpg';
+import Latrice from './images/Latrice.jpg';
+import Manila from './images/Manila2.jpg';
+import Bianca from './images/Bianca.jpg';
+import KimChi from './images/KimiChi.png';
+import Miz_Cracker from './images/Miz_Cracker.jpg';
+import Trinity from './images/Trinity3.jpg';
+import Valentina from './images/Valentina2.jpg';
+import BenDeLa from './images/BenDeLa.jpg';
+import Sasha from './images/Sasha.jpg';
+
+const images = [
+  Farrah,
+  Rupaul,
+  Trixie,
+  Latrice,
+  Manila,
+  Bianca,
+  KimChi,
+  Miz_Cracker,
+  Trinity,
+  Valentina,
+  BenDeLa,
+  Sasha
+];
+
+export default images;
