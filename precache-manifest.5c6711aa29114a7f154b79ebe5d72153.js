bffca1f7a7fcfe8b98186c5cbea0584eaf29bf19
NBCorrigan <nickb.corrigan@gmail.com>
2019-01-10 18:37:37 -0800

    Updates

---
+++ precache-manifest.5c6711aa29114a7f154b79ebe5d72153.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "461dbdada2a1fcdbbd001c90409f7865",
+    "url": "/rupaulclicky/static/media/Bianca.461dbdad.jpg"
+  },
+  {
+    "revision": "68ab5816f5d379a6b2bc",
+    "url": "/rupaulclicky/static/css/main.2d063283.chunk.css"
+  },
+  {
+    "revision": "de822213a7d64472361b",
+    "url": "/rupaulclicky/static/js/1.de822213.chunk.js"
+  },
+  {
+    "revision": "7619c1d1f6510d30de3d",
+    "url": "/rupaulclicky/static/js/runtime~main.7619c1d1.js"
+  },
+  {
+    "revision": "2779b9ec457d123ef42a341aa8629615",
+    "url": "/rupaulclicky/static/media/Farrah.2779b9ec.png"
+  },
+  {
+    "revision": "984d227a8f58689e37de7599ea4e31d1",
+    "url": "/rupaulclicky/static/media/Trixie.984d227a.jpg"
+  },
+  {
+    "revision": "b2d1f2b0e836a167dfb06ed081fb662c",
+    "url": "/rupaulclicky/static/media/Latrice.b2d1f2b0.jpg"
+  },
+  {
+    "revision": "68ab5816f5d379a6b2bc",
+    "url": "/rupaulclicky/static/js/main.68ab5816.chunk.js"
+  },
+  {
+    "revision": "a73783fb7f75f27d74e4d9d6abf0e51d",
+    "url": "/rupaulclicky/static/media/KimiChi.a73783fb.png"
+  },
+  {
+    "revision": "cbd868def7e63e1e1f5dc71a3469a707",
+    "url": "/rupaulclicky/static/media/Miz_Cracker.cbd868de.jpg"
+  },
+  {
+    "revision": "f571064f31ce163fdb4c7391fca708c9",
+    "url": "/rupaulclicky/static/media/Trinity3.f571064f.jpg"
+  },
+  {
+    "revision": "96ccc42f0725f3490406921e92052970",
+    "url": "/rupaulclicky/static/media/Valentina2.96ccc42f.jpg"
+  },
+  {
+    "revision": "43de899479aca80a0e2c487aea46d8f8",
+    "url": "/rupaulclicky/static/media/BenDeLa.43de8994.jpg"
+  },
+  {
+    "revision": "161b674701f9835d8db4887c3c2c36da",
+    "url": "/rupaulclicky/index.html"
+  }
+];
